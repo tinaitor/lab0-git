e9f87ebe11256ba8e800ac49f67152a29d292817
Jiroyed <ucheba55348@mail.ru>
2026-09-11 09:56:41 +0700

    Point2D

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,13 +14,30 @@ namespace lab0_git
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Point2D
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void AddX(int x)
+        {
+            X += x;
+        }
+        public void AddY(int y)
+        {
+            Y += y;
+        }
+    }
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            //kjhjk
-        }
+       public MainWindow()
+            {
+                InitializeComponent();
+            }
     }
 }
